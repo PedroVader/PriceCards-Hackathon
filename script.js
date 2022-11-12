dcb8bdb89b25831ced44446e0e2fbafdb47da977
Pedro <96238562+PedroVader@users.noreply.github.com>
2022-11-12 13:17:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var btnPro = document.getElementById("myBtnPro");
 var btnTeams = document.getElementById("myBtnTeams");
+
+// get the loading <button>
 var loadingBtn = document.getElementById("loadingBtn");
 
 // get the <button> element submit/request modal
@@ -17,6 +19,7 @@ var submitModal = document.getElementById("modalSubmit");
 var buttonClose = document.getElementsByClassName("modal-close")[0];
 var checkIcon = document.getElementById("checkIcon");
 
+// hidden icon check
 checkIcon.style.visibility = "hidden";
 
 //regular expressions for validate email
@@ -47,7 +50,9 @@ buttonClose.onclick = function () {
 }
 
 
-
+/* When the user clicks on <buton> "Submit", verifies the function of the input name 
+and email and if everything is correct access the second modal
+if the condition is not met, an error message is displayed and false is returned.    */
 btnSubmit.onclick = function () {
     //get the value of the input name/email
     var inputEmail = document.getElementById("inputEmail").value;
@@ -57,10 +62,11 @@ btnSubmit.onclick = function () {
         return false;
     } else {
         if (validEmail.test(inputEmail)) {
+            //create button loader
             this.innerHTML = "<button id='loadingBtn' class='button is-loading is-primary'></button>";
+            // check icon visible when the condition is true
             checkIcon.style.visibility = "visible";
             setTimeout(() => {
-                //create button loader
                 submitModal.style.display = "block";
                 modal.style.display = "none";
             }, 2000)
@@ -72,7 +78,7 @@ btnSubmit.onclick = function () {
 }
 
 
-
+//when the user clicks on "Ok" in the second modal, the page is closed and then reload
 btnRequestModal.onclick = function () {
     submitModal.style.display = "none";
     window.location.reload();
